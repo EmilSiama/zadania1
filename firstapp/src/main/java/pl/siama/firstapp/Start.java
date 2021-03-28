4c66ba1518eeba9bae613eab9cc785bc9118340d
EmilSiama <255822@student.pwr.edu.pl>
2021-03-28 18:03:29 +0200

    drugi program

--- firstapp/src/main/java/pl/siama/firstapp/Start.java
+++ firstapp/src/main/java/pl/siama/firstapp/Start.java
@@ -7,20 +7,42 @@ public class Start {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        System.out.println("pierwsza temperatura :");
-        int t1 = input.nextInt();
-        System.out.println("druga temperatura:");
-        int t2 = input.nextInt();
+        System.out.println("pierwsza liczba :");
+        int l1 = input.nextInt();
+        System.out.println("druga :");
+        int l2 = input.nextInt();
+        System.out.println("trzecia liczba :");
+        int l3 = input.nextInt();
+        System.out.println("czwarta :");
+        int l4 = input.nextInt();
+        System.out.println("piąta liczba :");
+        int l5 = input.nextInt();
+        System.out.println("szósta :");
+        int l6 = input.nextInt();
 
-        if (t1 < 100 || t2 < 100)
+        int[] tablica = {l1, l2, l3, l4, l5, l6};
+
+        for (int i = 0; i < tablica.length; i++)
         {
-            System.out.println("true");
-        }
-        else
+            if (i + 2 < tablica.length)
             {
-                System.out.println("false");
-            }
+                if (tablica[i] == 1)
+                {
+                    if (tablica[i + 1] == 2)
+                    {
+                        if (tablica[i + 2] == 3)
+                        {
+                            System.out.println("true");
+                        }
+                            else
+                            {
+                            System.out.println("false");
+                            }
 
+                    }
+                }
+            }
+        }
     }
 }
 
