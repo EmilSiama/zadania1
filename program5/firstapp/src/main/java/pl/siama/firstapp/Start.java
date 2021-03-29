96ad00470f7f13429e83073d87da6520b9fa385c
EmilSiama <255822@student.pwr.edu.pl>
2021-03-29 11:09:00 +0200

    dodanie programu 5

---
+++ program5/firstapp/src/main/java/pl/siama/firstapp/Start.java
@@ -0,0 +1,21 @@
+package pl.siama.firstapp;
+
+import java.util.Scanner;
+
+public class Start
+{
+
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        string program = new string();
+        program.Run(input);
+
+    }
+}
+
+
+
+
+
+
